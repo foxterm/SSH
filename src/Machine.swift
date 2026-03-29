f1548a985adf11ea8f8ff61258bb5a34de1b28a7
FoxTerm <foxterm@foxmail.com>
2026-03-29 23:24:59 +0800

    Update Machine.swift

--- src/Machine.swift
+++ src/Machine.swift
@@ -9,13 +9,10 @@ public class Machine {
     /// 容器类型（例如 Docker、Podman 等），用于在采集数据时适配不同的路径或逻辑
     public var container: ContainerType = .docker
 
-    /// 关联的底层 SSH 通信渠道，用于执行采集命令或读取 /proc 文件
-    let channel: Channel
+    let ssh: SSH
 
-    /// 初始化方法
-    /// - Parameter channel: 已经建立连接的 SSH 渠道实例
-    init(channel: Channel) {
-        self.channel = channel
+    init(ssh: SSH) {
+        self.ssh = ssh
     }
 
     deinit {
