ee0fe83def1b7f6b8d98a53913918933d527a9a6
FoxTerm <admin@foxterm.app>
2026-05-08 15:19:44 +0800

    Update WaitGroup.swift

--- Sources/Extension/WaitGroup.swift
+++ Sources/Extension/WaitGroup.swift
@@ -7,7 +7,7 @@ import libetos
 
 /// 等待组类，提供一种同步机制，用于等待多个并发任务全部完成
 /// 类似于 Go 语言中的 sync.WaitGroup
-public final class WaitGroup: Sendable {
+public final class WaitGroup: @unchecked Sendable {
     /// 全局共享的等待组实例
     public static let shared: WaitGroup = .init()
 
@@ -28,22 +28,22 @@ public final class WaitGroup: Sendable {
     }
 }
 
-public extension WaitGroup {
+extension WaitGroup {
     /// 增加等待组的计数器（默认为 1）
     /// 在启动一个新的并发任务前调用
     /// - Parameter n: 增加的数量
-    func add(_ n: Int32 = 1) {
+    public func add(_ n: Int32 = 1) {
         etos_sync_waitgroup_add(&_wg, n)
     }
 
     /// 减少等待组的计数器（通常在任务结束时调用）
     /// 当计数器归零时，会唤醒所有处于 `wait()` 状态的线程
-    func done() {
+    public func done() {
         etos_sync_waitgroup_done(&_wg)
     }
 
     /// 阻塞当前线程，直到等待组的计数器变为 0
-    func wait() {
+    public func wait() {
         etos_sync_waitgroup_wait(&_wg)
     }
 
@@ -51,7 +51,7 @@ public extension WaitGroup {
     /// 推荐用于简单的同步包裹，确保任务结束后计数器一定能递减
     /// - Parameter body: 需要执行的任务闭包
     /// - Returns: 闭包执行的返回值
-    func with<T>(_ body: () -> T) -> T {
+    public func with<T>(_ body: () -> T) -> T {
         add()
         defer {
             // 使用 defer 确保即便任务抛出异常也能正确执行 done()
@@ -61,7 +61,7 @@ public extension WaitGroup {
     }
 
     /// 针对无返回值闭包的等待组封装
-    func withVoid(_ body: () -> Void) {
+    public func withVoid(_ body: () -> Void) {
         with(body)
     }
 }
