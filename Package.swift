cf5d4f2ffa31723041f671d01e78cd658491ab15
FoxTerm <admin@foxterm.app>
2026-05-08 16:25:19 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
+            name: "OpenSSL",
             url: "https://github.com/Lakr233/openssl-spm.git",
             .upToNextMinor(from: "4.0.0")
         ),
