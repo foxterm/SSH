2e1748ddc241cb32199302dc4ecc2af7e6f8c0e9
费三量 <feisanliang@feisanliangdeMac-mini.local>
2026-08-25 22:00:40 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "SSH",
     platforms: [
         .macOS(.v14),
-        .iOS(.v17),
+        .iOS(.v16),
     ],
     products: [
         .library(name: "SSH", targets: ["SSH"]),
