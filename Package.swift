af2e4df93f2efe37f75baff91c7d43d7ad17726e
FoxTerm <foxterm@foxmail.com>
2026-09-09 01:36:03 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -46,7 +46,10 @@ let package = Package(
             ]
         ),
         .target(
-            name: "libetos"
+            name: "libetos",
+            dependencies: [
+                .product(name: "OpenSSL", package: "OpenSSL"),
+            ]
         ),
     ],
     swiftLanguageVersions: [.v5]
