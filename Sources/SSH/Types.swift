7dad71e4508fb4ad18d767af4ca8348c96ae11cf
FoxTerm <foxterm@foxmail.com>
2026-09-09 16:34:02 +0800

    Update Types.swift

--- Sources/SSH/Types.swift
+++ Sources/SSH/Types.swift
@@ -159,36 +159,3 @@ public enum Shout: Sendable {
         }
     }
 }
-
-/// 表示支持的网络代理协议类型枚举
-public enum ProxyType: String, CaseIterable, Codable, Sendable {
-    /// HTTP 代理协议 (HTTP CONNECT)
-    case http
-
-    /// SOCKS5 代理协议 (RFC 1928)
-    case socks5
-
-    /// 该代理协议的标准默认端口号（字符串形式）
-    public var portStr: String {
-        String(port)
-    }
-
-    /// 该代理协议的标准默认端口号（整数形式）
-    public var port: Int {
-        switch self {
-        case .http:
-            8080
-        case .socks5:
-            1080
-        }
-    }
-
-    public var raw: Int32 {
-        switch self {
-        case .http:
-            ETOS_PROXY_HTTP
-        case .socks5:
-            ETOS_PROXY_SOCKS5
-        }
-    }
-}
