35d237928db95b4c79ef0c298db55b470e254ce8
FoxTerm <foxterm@foxmail.com>
2026-09-11 12:10:47 +0800

    Update Crypto.swift

--- Sources/Crypto/Crypto.swift
+++ Sources/Crypto/Crypto.swift
@@ -2,14 +2,10 @@
 // Copyright (c) 2025-2026 foxterm.app
 // Created by foxterm@foxmail.com
 
-import Foundation
-import libtracked
-import OpenSSL
+import Foundationimport OpenSSL
 
 public final class Crypto: Sendable {
     public static let shared: Crypto = .init()
     public static let openssl_version = OPENSSL_VERSION_STR
-    init() {
-        openssl_mem_tracker_init()
-    }
+    init() {}
 }
