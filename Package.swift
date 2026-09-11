5813225aca67f2615a864e349bb54fabbbbe6d87
FoxTerm <foxterm@foxmail.com>
2026-09-11 12:10:45 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
         .library(name: "Extension", targets: ["Extension"]),
         .library(name: "Sync", targets: ["Sync"]),
         .library(name: "libetos", targets: ["libetos"]),
-        .library(name: "libtracked", targets: ["libtracked"]),
         .library(name: "Proxy", targets: ["Proxy"]),
     ],
     dependencies: [
@@ -29,7 +28,6 @@ let package = Package(
             dependencies: [
                 .target(name: "Extension"),
                 .product(name: "OpenSSL", package: "OpenSSL"),
-                .target(name: "libtracked"),
             ]
         ),
         .target(
@@ -39,7 +37,6 @@ let package = Package(
                 .target(name: "Proxy"),
                 .target(name: "Sync"),
                 .target(name: "libetos"),
-                .target(name: "libtracked"),
                 .product(name: "CSSH2", package: "libssh2"),
             ]
         ),
@@ -66,13 +63,6 @@ let package = Package(
             name: "libetos",
             dependencies: [
                 .product(name: "OpenSSL", package: "OpenSSL"),
-            ]
-        ),
-        .target(
-            name: "libtracked",
-            dependencies: [
-                .product(name: "OpenSSL", package: "OpenSSL"),
-                .product(name: "CSSH2", package: "libssh2"),
             ]
         ),
     ],
