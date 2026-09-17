5470b186301e0eb56ca05d43364b5e3ab22c694c
FoxTerm <foxterm@foxmail.com>
2026-09-17 20:03:33 +0800

    Update SSH.swift

--- Sources/SSH/SSH.swift
+++ Sources/SSH/SSH.swift
@@ -46,7 +46,7 @@ public class SSH {
     /// 用于同步并发任务的等待组
     let wait: WaitGroup = .init()
     /// 用于管理通道的轮询器
-    let channelPoll: ChannelPoll = .init()
+    let channelPoll: SSHChannelPoller = .init()
 
     /// 指向 libssh2_session 的原始 C 指针
     public internal(set) var rawSession: OpaquePointer?
